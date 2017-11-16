d58ecc07909e7c86d0a4b7af343a795bbf2d35c7
Josef Adelsberger <josef.adelsberger@hotmail.com>
2017-11-16 08:58:27 +0100

    test commit

--- Lave+/Login System Tut ecnrypted/Main.Designer.cs
+++ Lave+/Login System Tut ecnrypted/Main.Designer.cs
@@ -45,6 +45,7 @@
             this.lagerBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.lagerverwaltungDataSet = new Login_System_Tut_ecnrypted.LagerverwaltungDataSet();
             this.lagerTableAdapter = new Login_System_Tut_ecnrypted.LagerverwaltungDataSetTableAdapters.LagerTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.lagerBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.lagerverwaltungDataSet)).BeginInit();
@@ -155,11 +156,21 @@
             // 
             this.lagerTableAdapter.ClearBeforeFill = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(813, 516);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "main";
             this.Text = "Lager+";
@@ -188,5 +199,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn herstellerDataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn lagerstandDataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn einheitDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.Button button1;
     }
 }
