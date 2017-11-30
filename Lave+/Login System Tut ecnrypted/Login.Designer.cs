36ec1f4aaaa4787297d846c8cfabd3ea97590e17
Josef Adelsberger <josef.adelsberger@hotmail.com>
2017-11-30 11:01:33 +0100

    prolaps

--- Lave+/Login System Tut ecnrypted/Login.Designer.cs
+++ Lave+/Login System Tut ecnrypted/Login.Designer.cs
@@ -28,78 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_anmelden = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.usrTxt = new System.Windows.Forms.TextBox();
             this.passTxt = new System.Windows.Forms.TextBox();
+            this.lbl_benutzerlogin = new System.Windows.Forms.Label();
+            this.lbl_passwort_vergessen = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_anmelden
             // 
-            this.button1.Location = new System.Drawing.Point(358, 157);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 36);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Anmelden";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btn_anmelden.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_anmelden.Location = new System.Drawing.Point(323, 216);
+            this.btn_anmelden.Name = "btn_anmelden";
+            this.btn_anmelden.Size = new System.Drawing.Size(222, 36);
+            this.btn_anmelden.TabIndex = 3;
+            this.btn_anmelden.Text = "Anmelden";
+            this.btn_anmelden.UseVisualStyleBackColor = true;
+            this.btn_anmelden.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(274, 108);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(209, 122);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.Size = new System.Drawing.Size(108, 18);
             this.label1.TabIndex = 1;
             this.label1.Text = "Benutzername:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(296, 134);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(242, 150);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.Size = new System.Drawing.Size(75, 18);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Password:";
+            this.label2.Text = "Passwort:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // usrTxt
             // 
-            this.usrTxt.Location = new System.Drawing.Point(358, 105);
+            this.usrTxt.Location = new System.Drawing.Point(323, 122);
             this.usrTxt.Name = "usrTxt";
-            this.usrTxt.Size = new System.Drawing.Size(115, 20);
+            this.usrTxt.Size = new System.Drawing.Size(222, 20);
             this.usrTxt.TabIndex = 1;
             // 
             // passTxt
             // 
-            this.passTxt.Location = new System.Drawing.Point(358, 131);
+            this.passTxt.Location = new System.Drawing.Point(323, 148);
             this.passTxt.Name = "passTxt";
             this.passTxt.PasswordChar = '*';
-            this.passTxt.Size = new System.Drawing.Size(115, 20);
+            this.passTxt.Size = new System.Drawing.Size(222, 20);
             this.passTxt.TabIndex = 2;
+            // 
+            // lbl_benutzerlogin
+            // 
+            this.lbl_benutzerlogin.AutoSize = true;
+            this.lbl_benutzerlogin.Font = new System.Drawing.Font("MS Reference Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_benutzerlogin.Location = new System.Drawing.Point(317, 40);
+            this.lbl_benutzerlogin.Name = "lbl_benutzerlogin";
+            this.lbl_benutzerlogin.Size = new System.Drawing.Size(228, 35);
+            this.lbl_benutzerlogin.TabIndex = 5;
+            this.lbl_benutzerlogin.Text = "Benutzerlogin";
+            // 
+            // lbl_passwort_vergessen
+            // 
+            this.lbl_passwort_vergessen.AutoSize = true;
+            this.lbl_passwort_vergessen.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lbl_passwort_vergessen.Location = new System.Drawing.Point(320, 171);
+            this.lbl_passwort_vergessen.Name = "lbl_passwort_vergessen";
+            this.lbl_passwort_vergessen.Size = new System.Drawing.Size(108, 13);
+            this.lbl_passwort_vergessen.TabIndex = 6;
+            this.lbl_passwort_vergessen.Text = "Passwort vergessen?";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(748, 417);
+            this.label3.Location = new System.Drawing.Point(632, 417);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "AB-Soft";
+            this.label3.Size = new System.Drawing.Size(159, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Lagerverwaltung+, 2017, ABsoft";
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(803, 439);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_passwort_vergessen);
+            this.Controls.Add(this.lbl_benutzerlogin);
             this.Controls.Add(this.passTxt);
             this.Controls.Add(this.usrTxt);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_anmelden);
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
@@ -110,11 +138,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_anmelden;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox usrTxt;
         private System.Windows.Forms.TextBox passTxt;
+        private System.Windows.Forms.Label lbl_benutzerlogin;
+        private System.Windows.Forms.Label lbl_passwort_vergessen;
         private System.Windows.Forms.Label label3;
     }
 }
